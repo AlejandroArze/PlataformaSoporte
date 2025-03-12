af56cd0295dbd7fa0b7df821422afd83727cfd5d
Alejandro Arze <alejandrofernandoarze@gmail.com>
2025-03-12 14:14:28 -0400

    modificaciones para subir a servidor de Prueba

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-     baseUrl: "http://192.168.220.88:3001/api/v1",
-     baseUrlAlt: "http://192.168.220.88:3001"
+     baseUrl: "soportetecnicoapidev.cochabamba.bo/api/v1",
+     baseUrlAlt: "soportetecnicoapidev.cochabamba.bo"
 };
